dcbb9956538829f510bb3696398a88761670edde
Gerald Anderson <gerald@zapbangstudios.com>
2016-03-07 17:20:18 -0600

    client side ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+"use strict";
+
 var gulp = require("gulp"),
     $ = require("gulp-load-plugins")(),
     source = require("vinyl-source-stream"),
     browserify = require("browserify"),
     watchify = require("watchify"),
-    babelify = require("babelify");
+    babelify = require("babelify"),
+    fs = require("fs"),
+    path = require("path");
 
 gulp.task("scripts:server", () => {
 	return gulp.src("./src-server/**/*.js")
@@ -13,7 +17,59 @@ gulp.task("scripts:server", () => {
 });
 
 
-gulp.task("watch:scripts:server", () => {
-    return gulp.watch("./src-server/**/*.js", gulp.series("scripts:server"));
+gulp.task("watch:scripts:server", gulp.series(
+    "scripts:server",
+    () => gulp.watch(".src-server/**/*.js", gulp.series("scripts:server"))));
 
+gulp.task("watch:scripts:client", () => {
+
+    const files = fs.readdirSync("./src-client");
+    for (let i = 0; i < files.length; i ++) {
+        const file = files[i];
+        if (path.extname(file) !== ".js") 
+            continue;
+
+        initBundlerWatch(path.join("src-client", file));
+    }
+
+    return gulp.watch("./src-client/**/*.js")
+        .on("change", initBundlerWatch);
 });
+
+
+gulp.task("watch:scripts", gulp.parallel(
+    "watch:scripts:client",
+    "watch:scripts:server"));
+
+let bundlers = {};
+
+function initBundlerWatch(file) {
+    if (bundlers.hasOwnProperty(file))
+        return;
+
+    const bundler = createBundler(file);
+
+    bundlers[file] = bundler;
+
+    const watcher = watchify(bundler);
+    const filename = path.basename(file);
+
+    function bundle() {
+        return bundler
+        .bundle()
+        .on("error", error => console.error(error))
+        .pipe(source(filename))
+        .pipe(gulp.dest("./public/build"));
+    }
+
+    watcher.on("update", bundle);
+    watcher.on("time", time => console.log("Built client in ${time} ms"));
+
+    bundle();
+}
+
+function createBundler(file) {
+    const bundler =  browserify(file);
+    bundler.transform(babelify);
+    return bundler;
+}
---
+++ src-client/example_00.js
@@ -0,0 +1,9 @@
+
+import moment from "moment";
+import $ from "jquery";
+
+$("body").text(moment().format());
+
+
+console.log("HEY!");
+alert("So cool - whatever");
