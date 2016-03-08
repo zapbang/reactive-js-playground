f8e295f50311c13e5d0436ea595525d6ed746adc
Gerald Anderson <gerald@zapbangstudios.com>
2016-03-08 16:07:51 -0600

    After lesson 12

--- src-server/example_01_observables.js
+++ src-server/example_01_observables.js
@@ -62,12 +62,39 @@ function createSubscriber(tag) {
 function createInterval$(time) {
     return new Rx.Observable(observer => {
         let index = 0;
-        setInterval(() => {
+        let interval = setInterval(() => {
+            console.log(`Generating ${index}`);
             observer.next(index++);
         }, time);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
+}
+
+
+function take$(sourceObservable$, amount) {
+    return new Rx.Observable(observer => {
+
+        let count = 0;
+        const subscription = sourceObservable$.subscribe({
+            next(item) {
+                observer.next(item);
+                if (++count >= amount)
+                    observer.complete();
+            }, 
+            error(error) { observer.error(error); },
+            complete() { observer.complete(); }
+        });
+
+        return () => subscription.unsubscribe();
+
     });
 }
 
 const everySecond$ = createInterval$(1000);
-everySecond$.subscribe(createSubscriber("one"));
 
+const firstFiveSecond$ = take$(everySecond$, 5);
+
+const subscription = firstFiveSecond$.subscribe(createSubscriber("one"));
